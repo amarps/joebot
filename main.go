0703e29d858744b569eddd74084baa4ed19f5b62
amarps <amarpanjis@gmail.com>
2024-10-17 14:55:26 +0700

    remove unnecessary comment

--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"github.com/amarps/joebot/cmd"
-	// "github.com/labstack/echo"
-	// "github.com/labstack/echo/middleware"
-	// kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 func main() {
